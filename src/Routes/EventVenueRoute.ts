ad6b6cd6ce2092f299f5e1e8f5c4ca377d8aaf13
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-03-29 18:32:01 +0100

    feat: create EventVenue Service, Controller and Routes and integrated it into the app

---
+++ src/Routes/EventVenueRoute.ts
@@ -0,0 +1,24 @@
+import router, { Router } from "express";
+import { EventVenueController } from "../Controllers/EventVenueController";
+import { body } from "express-validator";
+
+const eventVenueRouter: Router = router();
+const eventVenueController = new EventVenueController();
+
+eventVenueRouter.get("/", eventVenueController.getAllEventVenues);
+eventVenueRouter.post(
+    "/",
+    [
+        body("country").notEmpty().withMessage("Event country is required"),
+        body("city").notEmpty().withMessage("Event city is required"),
+        body("state").notEmpty().withMessage("Event state is required"),
+        body("street").notEmpty().withMessage("Event street is required"),
+        body("building").notEmpty().withMessage("Event building is required"),
+    ],
+    eventVenueController.createEventVenue,
+);
+eventVenueRouter.get("/:eventVenueId", eventVenueController.getEventVenueById);
+eventVenueRouter.patch("/:eventVenueId", eventVenueController.updateEventVenueById);
+eventVenueRouter.delete("/:eventVenueId", eventVenueController.deleteEventVenueById);
+
+export default eventVenueRouter;
