c5546955c1263c2fbc8cc7816122d1b6f799fc74
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-04-02 21:12:36 +0100

    fix: make sure event visibility is added on creation and update tickets field validator

--- src/Controllers/EventController.ts
+++ src/Controllers/EventController.ts
@@ -35,6 +35,8 @@ export class EventController {
 
             const organizerId = (req.user as IUser)._id;
             const eventData = { ...req.body, organizerId };
+            console.log(eventData);
+            
             const newEvent = await this.eventService.createEvent(eventData);
             res.status(201).json({ event: newEvent });
         } catch (error) {
