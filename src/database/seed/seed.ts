32023ff420602514b0a750fa8790477bacef2eaa
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-05-05 01:41:09 +0100

    fix(seed): restructure data to match data model change'

--- src/database/seed/seed.ts
+++ src/database/seed/seed.ts
@@ -49,9 +49,6 @@ for (let i = 0; i < 50; i++) {
         type: faker.helpers.enumValue(EventTypes),
         venue: faker.location.streetAddress(),
         location: faker.helpers.enumValue(LocationTypes),
-        ticketTypes: faker.helpers.arrayElements(
-            eventTicketTypes,
-        ) as IEventTicketType[],
         organizerId: faker.helpers.arrayElement(users) as IUser,
         startDate: faker.date.soon(),
         endDate: faker.date.future(),
