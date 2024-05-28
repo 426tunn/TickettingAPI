fa9037467b7bb9fcf4e73fdc2a6e1dd44ce27469
426tunn <akanniagbolahan@gmail.com>
2024-05-28 22:12:13 +0100

    add annotations for EventTicketTypeRoute

--- src/Routes/EventTicketTypeRoute.ts
+++ src/Routes/EventTicketTypeRoute.ts
@@ -53,4 +53,146 @@ eventTicketTypeRouter.delete(
     eventTicketTypeController.deleteEventTicketTypeById,
 );
 
+/**
+ * @openapi
+ * /api/v1/ticket-types:
+ *    get:
+ *      tags:
+ *        - Ticket Types Management
+ *      summary: Get all ticket types
+ *      security: [{ bearerAuth: [] }]
+ *      responses:
+ *        200:
+ *         description: Successful get all ticket types
+ *        401:
+ *         description: Unauthorized
+ *        500:
+ *         description: Internal server error
+ */
+
+
+/**
+ * @openapi
+ * /api/v1/ticket-types/:eventTicketTypeId:
+ *    get:
+ *      tags:
+ *        - Ticket Types Management
+ *      summary: Get a ticket type by Id
+ *      security: [{ bearerAuth: [] }]
+ *      parameters:
+ *        - in: path
+ *          name: eventTicketTypeId
+ *          required: true
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *         description: Successful get a ticket type by id
+ *        401:
+ *         description: Unauthorized
+ *        403:
+ *         description: Forbidden
+ *        404:
+ *         description: Ticket type not found
+ *        500:
+ *         description: Internal server error
+ */
+
+/**
+ * @openapi
+ * /api/v1/events/{eventId}/ticket-types:
+ *   get:
+ *     tags:
+ *       - Ticket Types Management
+ *     summary: Get ticket types for an event by id
+ *     security: [{ bearerAuth: [] }]
+ *     parameters:
+ *       - in: path
+ *         name: eventId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful get ticket types for an event by id
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Event or ticket types not found
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @openapi
+ * /api/v1/ticket-types/{eventTicketTypeId}:
+ *   patch:
+ *     tags:
+ *       - Ticket Types Management
+ *     summary: Update a ticket type by id
+ *     security: [{ bearerAuth: [] }]
+ *     parameters:
+ *       - in: path
+ *         name: eventTicketTypeId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody: 
+ *       required: true
+ *       content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 type:
+ *                   type: string
+ *                 price:
+ *                   type: integer
+ *               example:
+ *                 type: General Admission
+ *                 price: 10
+ *     responses:
+ *       200:
+ *         description: Successful update ticket type by id
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Ticket type not found
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @openapi
+ * /api/v1/ticket-types/{eventTicketTypeId}:
+ *   delete:
+ *     tags:
+ *       - Ticket Types Management
+ *     summary: Delete a ticket type by id
+ *     security: [{ bearerAuth: [] }]
+ *     parameters:
+ *       - in: path
+ *         name: eventTicketTypeId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Successful delete ticket type by id
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Ticket type not found
+ *       500:
+ *         description: Internal server error
+ */
+
 export default eventTicketTypeRouter;
