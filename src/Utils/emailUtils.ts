a8290cc92d8dcd7e8eb698ae75c4ac7207450a5d
426tunn <akanniagbolahan@gmail.com>
2024-06-21 16:28:55 +0100

    fix: emailUtils

--- src/Utils/emailUtils.ts
+++ src/Utils/emailUtils.ts
@@ -22,33 +22,34 @@ export const sendUserVerifiedEmail = async (email: string, userId: string) => {
         await transporter.sendMail(mailOptions);
         logger.info("Verified email sent successfully");
     } catch (error) {
-        logger.error("Error sending email:", error);
+        console.error("Error sending email:", error);
         throw new Error("Error sending verified email");
     }
 };
 
 export const sendPasswordResetEmail = async (
     email: string,
-    verificationToken: string,
+    resetToken: string,
 ) => {
     try {
         const mailOptions = {
             from: Config.EMAIL,
             to: email,
-            subject: "Account Verification",
-            text: `You are receiving this email because you (or someone else) has requested to verify your account.
-            Please click the following link, or paste it into your browser to complete the process:
-            http://${Config.HOST_URL}/users/verify-email?token=${verificationToken}`,
-            html: `<p>You are receiving this email because you (or someone else) has requested to verify your account.</p>
-            <p>Please click the following link to complete the process:</p>
-            <a href="http://${Config.HOST_URL}/users/verify-email?token=${verificationToken}">Verify Account</a>`,
+            subject: "Password Reset",
+            text: `You are receiving this email because you (or someone else) has requested a password reset for your account.
+        Please click the following link, or paste it into your browser to complete the process:
+        
+        http://${Config.HOST_URL}/users/reset-password?resetToken=${resetToken}
+        reset: ${resetToken}
+        
+        If you did not request this, please ignore this email and your password will remain unchanged.`,
         };
 
         await transporter.sendMail(mailOptions);
-        logger.info("Verification email sent successfully");
+        logger.info("Email sent successfully");
     } catch (error) {
-        logger.error("Error sending email:", error);
-        throw new Error("Error sending verification email");
+        console.error("Error sending email:", error);
+        throw new Error("Error sending password reset email");
     }
 };
 
@@ -70,7 +71,7 @@ export const sendVerificationEmail = async (
         await transporter.sendMail(mailOptions);
         logger.info("Verification email sent successfully");
     } catch (error) {
-        logger.error("Error sending email:", error);
+        console.error("Error sending email:", error);
         throw new Error("Error sending verification email");
     }
 };
