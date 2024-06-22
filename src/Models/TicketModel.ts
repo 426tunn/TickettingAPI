e00c1debabb39ddcfb5152d11c52aa5f24aaa086
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-22 12:54:33 +0100

    refactor: combine hooks into one callback

--- src/Models/TicketModel.ts
+++ src/Models/TicketModel.ts
@@ -40,22 +40,16 @@ const ticketSchema = new Schema<ITicket>(
 
 const eventService = new EventService(EventModel);
 
-ticketSchema.post("save", async function (doc) {
-    const totalTickets = await TicketModel.countDocuments({
-        eventId: doc.eventId,
-    });
-    await eventService.updateEventById(doc.eventId.toString(), {
-        totalTickets: totalTickets,
-    });
-});
-
-ticketSchema.post(["deleteOne", "findOneAndDelete"], async function (doc) {
-    const totalTickets = await TicketModel.countDocuments({
-        eventId: doc.eventId,
-    });
-    await eventService.updateEventById(doc.eventId.toString(), {
-        totalTickets: totalTickets,
-    });
-});
+ticketSchema.post(
+    ["save", "deleteOne", "findOneAndDelete"],
+    async function (doc) {
+        const totalTickets = await TicketModel.countDocuments({
+            eventId: doc.eventId,
+        });
+        await eventService.updateEventById(doc.eventId.toString(), {
+            totalTickets: totalTickets,
+        });
+    },
+);
 const TicketModel = mongoose.model<ITicket>("Ticket", ticketSchema);
 export { ITicket, TicketModel };
