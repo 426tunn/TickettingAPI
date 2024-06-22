e5d659315487d8b96f11c1e132c6641a2168dc3d
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-22 11:45:44 +0100

    feat: implement admin routes for getting all events

--- src/Controllers/AdminController.ts
+++ src/Controllers/AdminController.ts
@@ -2,15 +2,47 @@ import { matchedData, validationResult } from "express-validator";
 import { EventService } from "../Services/EventService";
 import { NextFunction, Request, Response } from "express";
 import { IEventPaginationAndSortReq } from "../Types/RequestTypes";
+import { EventStatus } from "../Enums/EventStatus";
 
 export default class AdminController {
     constructor(private eventService: EventService) {}
+
+    public getAllEvents = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        try {
+            const page = parseInt((req.query.page as string) || "1");
+            const perPage = parseInt((req.query.perPage as string) || "9");
+            const events = await this.eventService
+                .getAllEvents({
+                    page,
+                    perPage,
+                    status: Object.values(EventStatus),
+                })
+                .populate(
+                    "organizerId",
+                    "-password -verificationToken -verificationExpire",
+                );
+            return res.status(200).json({
+                data: events,
+                message: "Events gotten",
+            });
+        } catch (error) {
+            if (error.statusCode && error.statusCode !== 500) {
+                return next(error);
+            }
+            return res.status(500).json({ error, message: error.message });
+        }
+    };
 
     public updateEventStatus = async (
         req: Request,
         res: Response,
         next: NextFunction,
     ) => {
+        // TODO: extract request detector into a middleware
         try {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
